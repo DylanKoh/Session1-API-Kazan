da12aec790f000df42618f56b4330e6cfa2ceae8
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-03-10 10:50:52 +0800

    Hmm

--- Session1Model.Designer.cs
+++ Session1Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Users\Asus\source\repos\Session1-API-Kazan\Session1Model.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Users\Dylan Koh\Source\Repos\DylanKoh\Session1-API-Kazan\Session1Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
